246af09a48b5d5e4d9f12d5a6c54a1f718c3d2a7
Nishibayashi Takuji <takuji@senchan.jp>
2011-12-05 13:08:14 +0900

    module based plugin

--- lib/Qpsmtpd/Plugin/JobQueue.pm
+++ lib/Qpsmtpd/Plugin/JobQueue.pm
@@ -3,6 +3,42 @@ use strict;
 use warnings;
 our $VERSION = '0.01';
 
+use parent qw/Qpsmtpd::Plugin/;
+
+use Class::Accessor::Lite (
+    new => 0,
+    rw  => [qw/engine conf/],
+);
+
+
+use Data::Validator;
+
+use Qpsmtpd::Constants;
+use Qpsmtpd::Plugin::JobQueue::Engine;
+
+sub register {
+    my ($self, $qp, @args) = @_;
+    #TODO read config?
+    my @conf = $self->qp->config('jobqueue');
+    $self->conf($self->parse_config(conf => \@conf));
+}
+
+sub parse_config {
+    state $validator = Data::Validator->new(
+        conf => {isa => 'ArrayRef'}
+    )->with(qw/Method/);
+    my ($class, $args) = $validator->validate(@_);
+    #TODO parse config
+    return {};
+}
+
+sub hook_queue {
+    my ($self, $transaction) = @_;
+    #TODO call queue method
+
+    return DECLINED;
+}
+
 1;
 __END__
 
--- lib/Qpsmtpd/Plugin/JobQueue/Engine.pm
+++ lib/Qpsmtpd/Plugin/JobQueue/Engine.pm
@@ -4,9 +4,17 @@ use warnings;
 use 5.10.0;
 
 use Carp ();
+use Data::Validator;
 
 sub queue { Carp::croak("Please implement queue method") }
 
+sub get_engine {
+    state $validator = Data::Validator->new(
+        config => {isa => 'HashRef'},
+    )->with(qw/Method/);
+    my ($class, $args) = $validator->validate(@_);
+    
+}
 
 
 1;
