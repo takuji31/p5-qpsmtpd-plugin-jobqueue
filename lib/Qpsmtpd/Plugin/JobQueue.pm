a60ae46f1fffdedb412d89869d03a4126aac246d
Nishibayashi Takuji <takuji@senchan.jp>
2011-12-05 11:46:57 +0900

    initial commit

---
+++ lib/Qpsmtpd/Plugin/JobQueue.pm
@@ -0,0 +1,32 @@
+package Qpsmtpd::Plugin::JobQueue;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Qpsmtpd::Plugin::JobQueue -
+
+=head1 SYNOPSIS
+
+  use Qpsmtpd::Plugin::JobQueue;
+
+=head1 DESCRIPTION
+
+Qpsmtpd::Plugin::JobQueue is
+
+=head1 AUTHOR
+
+Nishibayashi Takuji E<lt>takuji {at} senchan.jpE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
